e1f9bc4733f4d656793a3a20f35934b8f626b82d
KimJiSeong1994 <js_2109@naver.com>
2024-04-08 22:39:02 +0900

    Edit VanillaGNN model -- 24. 04. 08

--- src/model/VanillaGNN/model.py
+++ src/model/VanillaGNN/model.py
@@ -1,23 +1,32 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
 from src.model.VanillaGNN.args import arg
 from src.model.VanillaGNN.metrix import metric
+
+class VanillaGNNLayer(torch.nn.Module):
+    def __init__(self, dim_in, dim_out):
+        super().__init__()
+        self.linear = nn.Linear(dim_in, dim_out, bias = False)
+
+    def forward(self, x, adj):
+        x = self.linear(x)
+        x = torch.sparse.mm(adj, x)
+        return x
 
 class MLP(torch.nn.Module):
     def __init__(self, dim_in, dim_h, dim_out):
         super().__init__()
-        self.linear1 = nn.Linear(dim_in, dim_h)
-        self.linear2 = nn.Linear(dim_h, dim_out)
+        self.gnn1 = VanillaGNNLayer(dim_in, dim_h)
+        self.gnn2 = VanillaGNNLayer(dim_h, dim_out)
 
-    def forward(self, x):
-        x =  self.linear1(x)
+    def forward(self, x, A):
+        x =  self.gnn1(x, A)
         x = torch.relu(x)
-        x = self.linear2(x)
+        x = self.gnn2(x, A)
         return F.log_softmax(x, dim = 1)
 
-    def fit(self, data, epochs):
+    def fit(self, data, A, epochs):
         criterion = torch.nn.CrossEntropyLoss()
         optimizer = torch.optim.Adam(
             self.parameters(),
@@ -28,7 +37,7 @@ class MLP(torch.nn.Module):
         self.train()
         for epoch in range(epochs + 1):
             optimizer.zero_grad()
-            out = self(data.x)
+            out = self(data.x, A)
 
             loss = criterion(
                 out[data.train_mask],
@@ -57,12 +66,11 @@ class MLP(torch.nn.Module):
                     f'Epoch {epoch:>3} | Train Loss: {loss:.3f} | Train Acc: {acc*100:>5.2f}% | Val Loss: {val_loss:.2f} | Val Acc: {val_acc*100:.2f}%'
                 )
 
-    def test(self, data):
+    def test(self, data, A):
         self.eval()
-        out = self(data.x)
+        out = self(data.x, A)
         acc = metric.accuracy(
             out.argmax(dim = 1)[data.test_mask],
             data.y[data.test_mask]
         )
-
         return acc
--- src/model/VanillaGNN/trainer.py
+++ src/model/VanillaGNN/trainer.py
@@ -1,5 +1,9 @@
 if __name__ == '__main__' :
     import pandas as pd
+
+    import torch
+    from torch_geometric.utils import to_dense_adj
+
     from src.model.VanillaGNN import GetData
     from src.model.VanillaGNN.args import arg
     from src.model.VanillaGNN.model import MLP
@@ -13,17 +17,20 @@ if __name__ == '__main__' :
 
     df_x = pd.DataFrame(data.x.numpy())
     df_x['label'] = pd.DataFrame(data.y)
+    Ahat = to_dense_adj(data.edge_index)[0]
+    Ahat += torch.eye(len(Ahat))
 
     model = MLP(
         dim_in = dataset.num_features,
         dim_h = arg.HIDDEN_SIZE,
-        dim_out = dataset.num_classes
+        dim_out = dataset.num_classes,
     )
 
     model.fit(
         data,
+        Ahat,
         arg.EPOCHS
     )
 
-    test_acc = model.test(data)
+    test_acc = model.test(data, Ahat)
     print(f'VanillaGNN test accuracy: {test_acc*100:.2f} %')
