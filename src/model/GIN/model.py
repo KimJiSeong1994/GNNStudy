e2057b2caf245eed2e76d24a2117b223858e9085
KimJiSeong1994 <js_2109@naver.com>
2024-04-18 00:49:36 +0900

    ADD GIN model -- 24. 04. 17

--- src/model/GIN/model.py
+++ src/model/GIN/model.py
@@ -1,8 +1,57 @@
 import torch
+import torch.nn as nn
 import torch.nn.functional as F
 import torch_geometric.nn as gnn
+torch.manual_seed(42)
 
-from src.model.GIN.args import arg
-from src.model.GIN.metric import metric
+class GIN(nn.Module) :
+    def __init__(self, dim_h, num_classes) :
+        super(GIN, self).__init__()
+        self.conv1 = gnn.GINConv(
+            nn.Sequential(
+                nn.Linear(dim_h, dim_h),
+                nn.BatchNorm1d(dim_h),
+                nn.ReLU(),
+                nn.Linear(dim_h, dim_h),
+                nn.ReLU()
+            )
+        )
 
+        self.conv2 = gnn.GINConv(
+            nn.Sequential(
+                nn.Linear(dim_h, dim_h),
+                nn.BatchNorm1d(dim_h),
+                nn.ReLU(),
+                nn.Linear(dim_h, dim_h),
+                nn.ReLU()
+            )
+        )
 
+        self.conv3 = gnn.GINConv(
+            nn.Sequential(
+                nn.Linear(dim_h, dim_h),
+                nn.BatchNorm1d(dim_h),
+                nn.ReLU(),
+                nn.Linear(dim_h, dim_h),
+                nn.ReLU()
+            )
+        )
+
+        self.lin1 = nn.Linear(dim_h * 3, dim_h * 3)
+        self.lin2 = nn.Linear(dim_h * 3, num_classes)
+
+    def forward(self, x, edge_index, batch) :
+        h1 = self.conv1(x, edge_index)
+        h2 = self.conv1(h1, edge_index)
+        h3 = self.conv1(h2, edge_index)
+
+        h1 = gnn.global_add_pool(h1, batch)
+        h2 = gnn.global_add_pool(h2, batch)
+        h3 = gnn.global_add_pool(h3, batch)
+
+        h = torch.cat((h1, h2, h3), dim = 1)
+        h = self.lin1(h)
+        h = h.relu()
+        h = F.dropout(h, p = 0.5, training = self.training)
+        h = self.lin2(h)
+        return F.log_softmax(h, dim = 1)
--- src/model/GIN/trainer.py
+++ src/model/GIN/trainer.py
@@ -1,6 +1,12 @@
 if __name__ == '__main__' :
-    from src.model.GIN import GetData
+    import torch
+    import torch.nn as nn
     from torch_geometric.loader import DataLoader
+
+    from src.model.GIN import GetData
+    from src.model.GIN.model import GIN
+    from src.model.GIN.args import arg
+    from src.model.GIN.metric import metric
 
     loader = GetData.get()
     dataset = loader['dataset']
@@ -9,6 +15,64 @@ if __name__ == '__main__' :
     val_dataset = dataset[int(len(dataset) * 0.8): int(len(dataset) * 0.9)]
     test_dataset = dataset[int(len(dataset) * 0.9):]
 
+    for data in train_dataset :
+        print(data)
+
     train_loader = DataLoader(train_dataset, batch_size = 64, shuffle = True)
     val_loader = DataLoader(val_dataset, batch_size = 64, shuffle = True)
     test_loader = DataLoader(test_dataset, batch_size = 64, shuffle = True)
+
+    def train(model, loader):
+        criterion = torch.nn.CrossEntropyLoss()
+        optimizer = torch.optim.Adam(model.parameters(), lr=arg.LR)
+
+        model.train()
+        for epoch in range(arg.EPOCHS + 1):
+            total_loss = 0
+            acc = 0
+            val_loss = 0
+            val_acc = 0
+
+            for data in loader:
+                optimizer.zero_grad()
+                out = model(data.x, data.edge_index, data.batch)
+
+                loss = criterion(out, data.y)
+                total_loss += loss / len(loader)
+                acc += metric.accuracy(out.argmax(dim=1), data.y) / len(loader)
+
+                loss.backward()
+                optimizer.step()
+
+                val_loss, val_acc = test(model, val_loader)
+                if (epoch % 20 == 0):
+                    print(f'Epoch {epoch:>3} | Train loss: {total_loss:.2f} | Train Acc: {acc * 100:5.2f}%'
+                          f'Val Loss: {val_loss:2.f} | Val Acc: {val_acc * 100:.2f}%')
+
+        return model
+
+    @torch.no_grad()
+    def test(model, loader):
+        criterion = nn.CrossEntropyLoss()
+        model.eval()
+        loss = 0
+        acc = 0
+
+        for data in loader:
+            out = model(data.x, data.edge_index, data.batch)
+            loss += criterion(out, data.y) / len(loader)
+            acc += metric.accuracy(out.argmax(dim=1), data.y) / len(loader)
+
+        return loss, acc
+
+    NUM_CLASS = len(set(train_loader.dataset.y.detach().numpy()))
+    model = GIN(dim_h = 32, num_classes = NUM_CLASS)
+    model = train(model, train_loader)
+
+
+
+
+
+
+
+
